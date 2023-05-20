669049b1d67c692869ffd7abccdb62d682901e8e
Denin Paul <Denin.Paul@ibm.com>
2023-05-20 08:09:18 +0530

    change API service

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hotelapp/Utils/global.dart';
 import 'entries.dart';
 
 void main() {
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    azureURL = apiURL;
+
     return MaterialApp(
       initialRoute: '/',
       routes: {
